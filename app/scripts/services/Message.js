e120b0f0e020a6b0f5dc4c493059e826419689f8
Christian Waters <christianwaters1993@gmail.com>
2017-12-01 22:32:42 -0500

    send messages immediate update

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -6,11 +6,12 @@
 
     Message.getByRoomId = function(roomId) {
       // Filter the messages by their room ID.
-      var messageArray = [];
-      ref.orderByChild('roomId').equalTo(roomId).on('value', function(snapshot) {
-        messageArray.push(snapshot.val());
-      });
-      return messageArray;
+      return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+      // var messageArray = [];
+      // ref.orderByChild('roomId').equalTo(roomId).on('value', function(snapshot) {
+      //   messageArray.push(snapshot.val());
+      // });
+      // return messageArray;
     };
 
     Message.send = function(newMessage) {
